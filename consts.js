512e3c256ad5f75d4ef5fd02e9d7f6b06f113dd9
Murat Eroglu <murat51392@gmail.com>
2021-12-05 12:03:30 +0300

    parameter changed

--- consts.js
+++ consts.js
@@ -7,10 +7,10 @@ export const LANDER_HEIGHT = 26;
 export const GRAVITY = 3.711;
 export const ANGLE_TO_RADIAN = 0.017453292519943295;
 export const FPS = 30;
-export const PATH_LENGTH = 200;
+export const PATH_LENGTH = 300;
 export const POPULATION_SIZE = 1000;
-export const ELITE_PERC = 0.1;
-export const MUTATION_PERC = 5;
+export const ELITE_PERC = 0.16;
+export const MUTATION_PERC = 1;
 export const HISTORY_SIZE = 50;
 export const gameToCanvasCoordinate = (x, y, canvas) => {
     const widthRatio = WIDTH / canvas.scrollWidth;
